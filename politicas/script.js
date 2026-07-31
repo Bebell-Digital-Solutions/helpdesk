b497903008e9a858bf2d5d5c6b5fe9ec91015671
Bebell Digital Solutions 🚀 <148649418+Bebell-Digital-Solutions@users.noreply.github.com>
2026-07-31 17:24:07 -0400

    Implement back-to-top button and clone logo slides
    Added functionality for a back-to-top button and cloned partner logo slides on document ready.

--- politicas/script.js
+++ politicas/script.js
@@ -1,1 +1,32 @@
+$(document).ready(function () {
+            const $slides = $(".partner-logo-slide");
+            const cloneCount = 3;
+            for (let i = 0; i < cloneCount; i++) {
+                $slides.children().clone(true).appendTo($slides);
+            }
+        });
 
+        // Lógica del botón "Volver arriba"
+        const backToTopBtn = document.getElementById("backToTop");
+
+        window.addEventListener("scroll", () => {
+            if (window.scrollY > 300) {
+                backToTopBtn.classList.add("show");
+            } else {
+                backToTopBtn.classList.remove("show");
+            }
+        });
+
+        backToTopBtn.addEventListener("click", () => {
+            window.scrollTo({ top: 0, behavior: "smooth" });
+        });
+    </script>
+
+    <script>
+        (function(d, s, id){
+            var js, fjs = d.getElementsByTagName(s)[0];
+            if (d.getElementById(id)) return;
+            js = d.createElement(s); js.id = id;
+            js.src = 'https://api.helpdesk.icu/widget2/load?id=b248d1b5-317e-3e0d-a6db-c255c207060b&r=' + encodeURIComponent(window.location);
+            fjs.parentNode.insertBefore(js, fjs);
+        }(document, 'script', 'anw2-sdk-tdFIsn4xDT6m28JVwgcGCw'));
